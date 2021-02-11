6479a5915d29824e0c390397add2bb0ac25f4a2f
Memet <meavci@gmail.com>
2021-02-10 22:12:44 -0500

    header edded

--- create-http-server.js
+++ create-http-server.js
@@ -1,5 +1,7 @@
 const http=require('http');
 const myServer=http.createServer((req,res)=>{
+  console.log("A Request from : ",req.url);
+    console.log("A Request header : ",req.header);
   res.writeHead(200,{'content-type':'text/html;charset=utf-8'})
   res.write('<b>Hello World from çöşlüğ HTTP SERVER..');
   res.end();
