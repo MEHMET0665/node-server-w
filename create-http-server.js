03388dff50d176f934df75197f295cfbbf7d8efb
Memet <meavci@gmail.com>
2021-02-10 21:31:08 -0500

    add sethead

--- create-http-server.js
+++ create-http-server.js
@@ -1,7 +1,8 @@
 const http=require('http');
 const myServer=http.createServer((req,res)=>{
-  res.write('<b>Hello World from HTTP SERVER..');
-  res.end(asa);
+  res.writeHead(200,{'content-type':'text/html;charset=utf-8'})
+  res.write('<b>Hello World from çöşlüğ HTTP SERVER..');
+  res.end();
 })
 
 myServer.listen(3005)
