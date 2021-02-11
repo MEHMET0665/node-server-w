6fd3a334aa176452ea7b4c09388bcdf70ea0ed48
Memet <meavci@gmail.com>
2021-02-10 23:07:38 -0500

    create function html

--- server-file.js
+++ server-file.js
@@ -1,12 +1,13 @@
-const fs=require('fs')
-const http=require('http')
+const fs = require('fs')
+const http = require('http')
 
-const myCustomServer=http.createServer((req,res)=>{
-  fs.readFile('test.html',(err,data)=>{
-    if (err){
-      console.log(err)
-    }
-    res.end(data)
-  })
+//GET - POST - PUT - DELETE
+// https://domainname.com/login?user=yourname&password=12345
+const myCustomServer= http.createServer((request,res)=>{
+    fs.readFile('test.html',(err,data)=>{
+        if (err) {console.log(err);}
+        res.end(data);
+    })
 })
-myCustomServer.listen(3001)
+
+myCustomServer.listen("3001")
